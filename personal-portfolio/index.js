6f6b8dee82961ea8073d53258e8f9c1fcbe3ff4e
unknown <rdogan8891@gmail.com>
2022-07-09 00:47:40 +0200

    more about me button has been added

--- personal-portfolio/index.js
+++ personal-portfolio/index.js
@@ -62,3 +62,6 @@ function erase() {
 document.addEventListener("DOMContentLoaded", function() { // On DOM Load initiate the effect
   if(textArray.length) setTimeout(type, newTextDelay + 250);
 });
+
+
+
