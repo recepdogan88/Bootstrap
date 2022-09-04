2aafba8aaed8aeb5562500495f447072f79ae190
Recep Dogan <rdogan8891@gmail.com>
2022-09-04 18:09:23 +0200

    certificates have been removed

--- personal-portfolio/index.js
+++ personal-portfolio/index.js
@@ -63,5 +63,12 @@ document.addEventListener("DOMContentLoaded", function() { // On DOM Load initia
   if(textArray.length) setTimeout(type, newTextDelay + 250);
 });
 
-
+document.addEventListener("click",function (e){
+  if(e.target.classList.contains("gallery-item")){
+      const src = e.target.getAttribute("src");
+      document.querySelector(".modal-img").src = src;
+      const myModal = new bootstrap.Modal(document.getElementById('gallery-modal'));
+      myModal.show();
+  }
+})
 
